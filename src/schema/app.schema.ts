7665204f364a207c1eb8b9b0e48b8a269700cdfd
AntonioMrtz <antonio@metrica-sports.com>
2025-12-01 22:21:09 +0100

    fix: 2025 as first option on project init

--- src/schema/app.schema.ts
+++ src/schema/app.schema.ts
@@ -1,5 +1,5 @@
 const APP_NAME = 'adventjs-cli';
-const APP_VERSION = '1.6.0';
+const APP_VERSION = '1.6.1';
 
 const APP_DESCRIPTION = `AdventJS CLI Generator – Spin up your AdventJS challenges in seconds! 🎄⚡
 
@@ -11,6 +11,6 @@ const APP_DESCRIPTION = `AdventJS CLI Generator – Spin up your AdventJS challe
 Focus on solving the challenges, not setting them up! 🚀`;
 
 const CURRENT_YEAR = '2025';
-const SUPPORTED_YEARS = ['2024', '2025'];
+const SUPPORTED_YEARS = [CURRENT_YEAR, '2024'];
 
 export { APP_NAME, APP_VERSION, APP_DESCRIPTION, CURRENT_YEAR, SUPPORTED_YEARS };
